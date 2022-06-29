896da984272212b0e9b40985b6e859459f3fbc1f
rmpinedo <rmpinedo@cic.es>
2022-06-29 13:09:38 +0200

    resto de rutas

--- functions/app/index.js
+++ functions/app/index.js
@@ -36,34 +36,55 @@ export default function expressApp(functionName) {
         <p>Choose a route:</p>
 
         <div>
-          <a href='/.netlify/functions/${functionName}/users'>View /users route</a>
+        <a href='/.netlify/functions/${functionName}/rutasEncontradas'>View /rutasEncontradas route</a>
+        </div>
+
+
+        <div>
+          <a href='/.netlify/functions/${functionName}/catalogo'>View /catalogo route</a>
         </div>
 
         <div>
-          <a href='/.netlify/functions/${functionName}/hello'>View /hello route</a>
+          <a href='/.netlify/functions/${functionName}/catalogo/salud'>View /catalogo/salud route</a>
         </div>
 
         <div>
-          <a href='/.netlify/functions/${functionName}/catalogo/ocioentretenimiento/centrosocial'>View /ocioentretenimiento/centrosocial route</a>
+          <a href='/.netlify/functions/${functionName}/catalogo/salud/centrosalud'>View /catalogo/salud/centrosalud route</a>
         </div>
-
-        <br/>
-        <br/>
 
         <div>
-          <a href='/'>
-            Go back to demo homepage
-          </a>
-        </div>
+        <a href='/.netlify/functions/${functionName}/catalogo/salud/hospital'>View /catalogo/salud/hospital route</a>
+      </div>
 
-        <br/>
-        <br/>
+      <div>
+        <a href='/.netlify/functions/${functionName}/catalogo/salud/farmacia'>View /catalogo/salud/farmacia route</a>
+      </div>
+
+      <div>
+      <a href='/.netlify/functions/${functionName}/catalogo/ocioentretenimiento'>View /catalogo/ocioentretenimiento route</a>
+    </div>
 
         <div>
-          <a href='https://github.com/DavidWells/netlify-functions-express' target='_blank'>
-            See the source code on github
-          </a>
+          <a href='/.netlify/functions/${functionName}/catalogo/ocioentretenimiento/centrosocial'>View /catalogo/ocioentretenimiento/centrosocial route</a>
         </div>
+
+        <div>
+          <a href='/.netlify/functions/${functionName}/catalogo/ocioentretenimiento/parques'>View /catalogo/ocioentretenimiento/parques route</a>
+        </div>
+
+        <div>
+        <a href='/.netlify/functions/${functionName}/catalogo/serviciomunicipal'>View /catalogo/serviciomunicipal route</a>
+      </div>
+
+      <div>
+        <a href='/.netlify/functions/${functionName}/catalogo/serviciomunicipal/oficina'>View /catalogo/serviciomunicipal/oficina route</a>
+      </div>
+
+      <div>
+        <a href='/.netlify/functions/${functionName}/catalogo/serviciomunicipal/empresa'>View /catalogo/serviciomunicipal/empresa route</a>
+      </div>
+    
+    
       </body>
     </html>
   `
@@ -81,6 +102,432 @@ export default function expressApp(functionName) {
         },
       ],
     })
+  })
+
+  router.get('/catalogo/ocioentretenimiento', (req, res) => {
+    res.json(
+
+    )
+  })
+
+  router.get('/catalogo/salud', (req, res) => {
+    res.json(
+      [
+        [{
+          "Salud": [{
+            "id": 0,
+            "nombre": "Hospital",
+            "tab": "HospitalDetallePage",
+            "ruta": "/tabs/DestinoPage/SaludDetallePage/HospitalDetallePage",
+            "subdirecciones": [{
+              "id": "1001",
+              "nombre": "H. Valdecilla",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }, {
+              "id": "1002",
+              "nombre": "Sta. Clotilde",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }
+            ]
+          }, {
+            "id": 1,
+            "nombre": "Centro Salud",
+            "tab": "CentroSaludDetallePage",
+            "ruta": "/tabs/DestinoPage/SaludDetallePage/CentroSaludDetallePage",
+            "subdirecciones": [{
+              "id": "2001",
+              "nombre": "CS Alta",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }, {
+              "id": "2002",
+              "nombre": "CS Isabel II",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }
+            ]
+          }, {
+            "id": 2,
+            "nombre": "Farmacia",
+            "tab": "FarmaciaDetallePage",
+            "ruta": "/tabs/DestinoPage/SaludDetallePage/FarmaciaDetallePage",
+            "subdirecciones": [{
+              "id": "3001",
+              "nombre": "Farmacia López",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }, {
+              "id": "3002",
+              "nombre": "Farmacia Ruiz",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }
+            ]
+          }
+          ]
+        }]
+      ]
+
+    )
+  })
+
+  router.get('/catalogo/salud/centrosalud', (req, res) => {
+    res.json(
+      [{
+        "id": 1,
+        "nombre": "Centro Salud",
+        "subdirecciones": [{
+          "id": "2001",
+          "nombre": "CS Alta",
+          "lon": -3.7962913513183594,
+          "lat": 43.47076747496938
+        }, {
+          "id": "2002",
+          "nombre": "CS Isabel II",
+          "lon": -3.7962913513183594,
+          "lat": 43.47076747496938
+        }
+        ]
+      }]
+
+    )
+  })
+
+  router.get('/catalogo/salud/hospital', (req, res) => {
+    res.json(
+
+      [{
+        "id": 0,
+        "nombre": "Hospital",
+        "tab": "HospitalDetallePage",
+        "ruta": "/tabs/DestinoPage/SaludDetallePage/HospitalDetallePage",
+        "subdirecciones": [{
+          "id": "1001",
+          "nombre": "H. Valdecilla",
+          "lon": -3.7962913513183594,
+          "lat": 43.47076747496938
+        }, {
+          "id": "1002",
+          "nombre": "Sta. Clotilde",
+          "lon": -3.7962913513183594,
+          "lat": 43.47076747496938
+        }
+        ]
+      }]
+
+    )
+  })
+
+  router.get('/catalogo/salud/farmacia', (req, res) => {
+    res.json(
+      [{
+        "id": 2,
+        "nombre": "Farmacia",
+        "subdirecciones": [{
+          "id": "3001",
+          "nombre": "Farmacia López",
+          "lon": -3.7962913513183594,
+          "lat": 43.47076747496938
+        }, {
+          "id": "3002",
+          "nombre": "Farmacia Ruiz",
+          "lon": -3.7962913513183594,
+          "lat": 43.47076747496938
+        }
+        ]
+      }]
+
+    )
+  })
+
+  router.get('/catalogo/serviciomunicipal', (req, res) => {
+    res.json(
+      [
+        [{
+          "serviciomunicipal": [{
+            "id": 3,
+            "nombre": "Oficina",
+            "tab": "OficinaDetallePage",
+            "ruta": "/tabs/DestinoPage/ServicioMunicipalDetallePage/OficinaDetallePage",
+            "subdirecciones": [{
+              "id": "4001",
+              "nombre": "Ayuntamiento",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }, {
+              "id": "4002",
+              "nombre": "Of. Turismo",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }
+            ]
+          }, {
+            "id": 4,
+            "nombre": "Empresa",
+            "tab": "EmpresaDetallePage",
+            "ruta": "/tabs/DestinoPage/ServicioMunicipalDetallePage/EmpresaDetallePage",
+            "subdirecciones": [{
+              "id": "5001",
+              "nombre": "TUS",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }, {
+              "id": "5002",
+              "nombre": "Aguas",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }
+            ]
+          }
+          ]
+        }
+        ]
+      ]
+
+    )
+  })
+
+  router.get('/catalogo/serviciomunicipal/oficina', (req, res) => {
+    res.json(
+      [{
+        "id": 3,
+        "nombre": "Oficina",
+        "tab": "OficinaDetallePage",
+        "ruta": "/tabs/DestinoPage/ServicioMunicipalDetallePage/OficinaDetallePage",
+        "subdirecciones": [{
+          "id": "4001",
+          "nombre": "Ayuntamiento",
+          "lon": -3.7962913513183594,
+          "lat": 43.47076747496938
+        }, {
+          "id": "4002",
+          "nombre": "Of. Turismo",
+          "lon": -3.7962913513183594,
+          "lat": 43.47076747496938
+        }
+        ]
+      }]
+
+    )
+  })
+
+  router.get('/catalogo/serviciomunicipal/empresa', (req, res) => {
+    res.json(
+      [{
+        "id": 4,
+        "nombre": "Empresa",
+        "tab": "EmpresaDetallePage",
+        "ruta": "/tabs/DestinoPage/ServicioMunicipalDetallePage/EmpresaDetallePage",
+        "subdirecciones": [{
+          "id": "5001",
+          "nombre": "TUS",
+          "lon": -3.7962913513183594,
+          "lat": 43.47076747496938
+        }, {
+          "id": "5002",
+          "nombre": "Aguas",
+          "lon": -3.7962913513183594,
+          "lat": 43.47076747496938
+        }
+        ]
+      }]
+
+    )
+  })
+
+  router.get('/rutasEncontradas', (req, res) => {
+    res.json(
+      [
+        {
+          "nombreRuta": "Ruta 1",
+          "recorrido": 430,
+          "desnivel": 20,
+          "tiempo": "1h 20m"
+        },
+        {
+          "nombreRuta": "Ruta 2",
+          "recorrido": 400,
+          "desnivel": 22,
+          "tiempo": "1h 32m"
+        }
+      ]
+
+    )
+  })
+
+  router.get('/catalogo/ocioentretenimiento', (req, res) => {
+    res.json(
+      [
+        [{
+          "ocioentretenimiento": [{
+            "id": 5,
+            "nombre": "Parques",
+            "tab": "ParqueDetallePage",
+            "ruta": "/tabs/DestinoPage/OcioEntretenimientoDetallePage/ParqueDetallePage",
+            "subdirecciones": [{
+              "id": "6001",
+              "nombre": "Parque las Llamas",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }, {
+              "id": "6002",
+              "nombre": "Jardines Piquío",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }
+            ]
+          }, {
+            "id": 5,
+            "nombre": "Centro social",
+            "tab": "CentroSocialDetallePage",
+            "ruta": "/tabs/DestinoPage/OcioEntretenimientoDetallePage/CentroSocialDetallePage",
+            "subdirecciones": [{
+              "id": "5001",
+              "nombre": "CSoc Calle Alta",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }, {
+              "id": "5002",
+              "nombre": "CSoc Gral. Dávila",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }
+            ]
+          }
+          ]
+        }
+        ]
+      ]
+
+    )
+  })
+
+  router.get('/catalogo', (req, res) => {
+    res.json(
+      [
+        [{
+          "Salud": [{
+            "id": 0,
+            "nombre": "Hospital",
+            "subdirecciones": [{
+              "id": "1001",
+              "nombre": "H. Valdecilla",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }, {
+              "id": "1002",
+              "nombre": "Sta. Clotilde",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }
+            ]
+          }, {
+            "id": 1,
+            "nombre": "Centro Salud",
+            "subdirecciones": [{
+              "id": "2001",
+              "nombre": "CS Alta",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }, {
+              "id": "2002",
+              "nombre": "CS Isabel II",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }
+            ]
+          }, {
+            "id": 2,
+            "nombre": "Farmacia",
+            "subdirecciones": [{
+              "id": "3001",
+              "nombre": "Farmacia López",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }, {
+              "id": "3002",
+              "nombre": "Farmacia Ruiz",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }
+            ]
+          }
+          ]
+        }, {
+          "ServiciosMunicipales": [{
+            "id": 3,
+            "nombre": "Oficina",
+            "subdirecciones": [{
+              "id": "4001",
+              "nombre": "Ayuntamiento",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }, {
+              "id": "4002",
+              "nombre": "Of. Turismo",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }
+            ]
+          }, {
+            "id": 4,
+            "nombre": "Empresa",
+            "subdirecciones": [{
+              "id": "5001",
+              "nombre": "TUS",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }, {
+              "id": "5002",
+              "nombre": "Aguas",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }
+            ]
+          }
+          ]
+        }, {
+          "OcioEntretenimiento": [{
+            "id": 5,
+            "nombre": "Parques",
+            "subdirecciones": [{
+              "id": "6001",
+              "nombre": "Parque las Llamas",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }, {
+              "id": "6002",
+              "nombre": "Jardines Piquío",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }
+            ]
+          }, {
+            "id": 5,
+            "nombre": "Centro social",
+            "subdirecciones": [{
+              "id": "5001",
+              "nombre": "CSoc Calle Alta",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }, {
+              "id": "5002",
+              "nombre": "CSoc Gral. Dávila",
+              "lon": -3.7962913513183594,
+              "lat": 43.47076747496938
+            }
+            ]
+          }
+          ]
+        }
+        ]
+      ]
+
+
+
+    )
   })
 
   router.get('/catalogo/ocioentretenimiento/centrosocial', (req, res) => {
@@ -101,6 +548,29 @@ export default function expressApp(functionName) {
         }
         ]
       }]
+
+    )
+  })
+
+  router.get('/catalogo/ocioentretenimiento/parques', (req, res) => {
+    res.json(
+      [{
+        "id": 5,
+        "nombre": "Parques",
+        "subdirecciones": [{
+          "id": "6001",
+          "nombre": "Parque las Llamas",
+          "lon": -3.7962913513183594,
+          "lat": 43.47076747496938
+        }, {
+          "id": "6002",
+          "nombre": "Jardines Piquío",
+          "lon": -3.7962913513183594,
+          "lat": 43.47076747496938
+        }
+        ]
+      }]
+
 
     )
   })
